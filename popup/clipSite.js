d54f2e567b7564a69b2d7d48b5dd3a2fc83a6040
donykevin214 <donykevin214@gmail.com>
2020-05-14 10:47:23 +1000

    Added code to download selected text

--- popup/clipSite.js
+++ popup/clipSite.js
@@ -35,9 +35,44 @@ function notify(message) {
     if (message.type == "display.md") {
         document.getElementById("md").value = message.markdown;
         document.getElementById("md").title = message.article.title;
+        document.getElementById("md").addEventListener("select", select);
+        document.getElementById("md").addEventListener("change", select);
+        document.getElementById("md").addEventListener("blur", select);
+        document.getElementById("md").addEventListener("mouseup", select);
+        document.getElementById("md").addEventListener("keyup", select);
+        document.getElementById("md").addEventListener("click", select);
         document.getElementById("download").addEventListener("click", download);
+        document.getElementById("downloadSelection").addEventListener("click", downloadSelection);
         document.getElementById("container").style.display = 'flex';
         document.getElementById("spinner").style.display = 'none';
     }
 }
 
+function select (event) {
+    var start = event.currentTarget.selectionStart;
+    var finish = event.currentTarget.selectionEnd;
+    var a = document.getElementById("downloadSelection");
+
+    if (start != finish) {
+        a.style.display = "block";
+    }
+    else {
+        a.style.display = "none";
+    }
+}
+
+function downloadSelection(e) {
+    e.preventDefault();
+    var md = document.getElementById("md");
+    var start = md.selectionStart;
+    var finish = md.selectionEnd;
+    var selection = md.value.substring(start, finish);
+    
+    var message = {
+        type: "download",
+        markdown: selection,
+        title: md.title
+    };
+
+    browser.runtime.sendMessage(message);
+}
