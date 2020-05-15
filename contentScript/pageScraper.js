15b5a794d34c47a15b694c3a7c77f62b7e57f933
donykevin214 <donykevin214@gmail.com>
2020-05-15 13:07:11 +1000

    Check for existing base element and update if necessary
    fixes https://github.com/deathau/markdown-clipper/issues/6

--- contentScript/pageScraper.js
+++ contentScript/pageScraper.js
@@ -1,11 +1,17 @@
 function notifyExtension() {
     // if the document doesn't have a "base" element make one
     // this allows the DOM parser in future steps to fix relative uris
-    if (document.head.getElementsByTagName('base').length == 0) {
-        let baseEl = document.createElement('base');
-        // use the current uri
+    let baseEl = document.createElement('base');
+    
+    // check for a existing base elements
+    let baseEls = document.head.getElementsByTagName('base');
+    if (baseEls.length > 0) { baseEl = baseEls[0]; }
+    // if we don't find one, append this new one.
+    else { document.head.append(baseEl); }
+    
+    // if the base element doesn't have a href, use the current location
+    if (!baseEl.getAttribute('href')) {
         baseEl.setAttribute('href', window.location.href);
-        document.head.append(baseEl);
     }
 
     // get the content of the page as a string
