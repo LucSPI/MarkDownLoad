9a3df8d1e000b5bd36f750183686c9f30d6fb48a
donykevin214 <donykevin214@gmail.com>
2022-10-25 20:58:02 -0600

    Don't keep `del` tag as it has a Markdown equivalent

--- src/background/background.js
+++ src/background/background.js
@@ -21,7 +21,7 @@ function turndown(content, options, article) {
 
   turndownService.use(turndownPluginGfm.gfm)
 
-  turndownService.keep(['iframe', 'sub', 'sup', 'u', 'ins', 'del', 'small', 'big']);
+  turndownService.keep(['iframe', 'sub', 'sup', 'u', 'ins', 'small', 'big']);
 
   let imageList = {};
   // add an image rule
